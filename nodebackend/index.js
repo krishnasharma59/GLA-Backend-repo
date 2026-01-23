eb68228d2231653f17c5fb26c91e95ec7bde5fc8
krishna <krishnaasglider@gmail.com>
2026-01-23 13:35:24 +0530

    [Script] Runs Assignment Solution

--- nodebackend/index.js
+++ nodebackend/index.js
@@ -1,5 +1,28 @@
-let {wordCount} = require("./Question1.js");
-wordCount();
+// Question 1
 
-let {wordCap} = require("./Question2.js");
-wordCap();
+// let {wordCount} = require("./Question1.js");
+// wordCount();
+
+
+//Question 2
+
+// let {wordCap,revStr, countVowels} = require("./Question2.js");
+// wordCap();
+// revStr();
+// countVowels();
+
+
+//Question 3
+
+// let {logOS} = require("./question3.js");
+// logOS();
+
+// Question 4
+
+let {writeFile,readFile,updateFile,deleteFile} = require("./question4.js");
+//Don't run writeFile() and deleteFile() together as they work on same file
+//first writeFile() then deleteFile()
+writeFile();
+readFile();
+updateFile();
+deleteFile();
